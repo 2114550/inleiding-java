4a57f708ee24f754fd6e3172c3cf5c2fde73d62c
2114550 <2114550@talnet.nl>
2019-09-04 12:52:54 +0200

    Theorie Hoofdstuk

---
+++ src/h03/h03o01.java
@@ -0,0 +1,16 @@
+package h03;
+import java.awt.*;
+import java.applet.*;
+
+public class h03o01 extends Applet {
+
+    public void init() {
+        setBackground(Color.magenta);
+    }
+    public void paint(Graphics g) {
+        g.setColor(Color.white);
+        g.drawString("Dit hoofdstuk was theorie, er waren geen opdrachten.", 100, 70 );
+    }
+
+}
+
