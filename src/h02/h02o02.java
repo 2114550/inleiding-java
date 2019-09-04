9178ff8e1983636cb3e442397bfc767d608fcd72
2114550 <2114550@talnet.nl>
2019-09-04 11:51:07 +0200

    Opdracht 2 afgerond

--- src/h02/h02o02.java
+++ src/h02/h02o02.java
@@ -9,9 +9,9 @@ public class h02o02 extends Applet {
     }
     public void paint(Graphics g) {
         g.setColor(Color.blue);
-        g.drawString("Wahid", 100, 60 );
+        g.drawString("Wahid", 100, 70 );
         g.setColor(Color.red);
-        g.drawString("Mamedov", 100, 70);
+        g.drawString("Mamedov", 100, 80);
     }
 
 }
