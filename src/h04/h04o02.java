e4c9d9cc2576c59139b7481680bd0b018b2e225b
2114550 <2114550@talnet.nl>
2019-09-05 13:00:20 +0200

    Geordent

--- src/h04/h04o02.java
+++ src/h04/h04o02.java
@@ -11,11 +11,19 @@ public class h04o02 extends Applet {
 
     public void paint(Graphics g) {
         g.setColor(Color.black);
+
+        //House body
         g.drawRect(100,250,200,200);
+
+        //House roof
         g.drawLine(100,250,300,250);
         g.drawLine(100,250,200,100);
         g.drawLine(300,250,200,100);
+
+        //House door
         g.drawRect(130,350,50,100);
+
+        //House window
         g.drawRect(225,350,50,50);
     }
 }
