f0eadc0c084392fde53249b1bb2d99b31c3213bb
2114550 <2114550@talnet.nl>
2019-09-05 14:39:25 +0200

    Nog niks gedaan

---
+++ src/h04/h04po.java
@@ -0,0 +1,15 @@
+package h04;
+
+import java.awt.*;
+import java.applet.*;
+
+public class h04po extends Applet {
+
+    public void init() {
+        setBackground(Color.white);
+    }
+
+    public void paint(Graphics g) {
+
+    }
+}
