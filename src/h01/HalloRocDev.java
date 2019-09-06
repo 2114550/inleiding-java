df2ba1eef66aafca4bcea7fb502e9732492e49a5
2114550 <2114550@talnet.nl>
2019-09-06 13:28:53 +0200

    HOOFDSTUK 1

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Opdracht 1");
+        System.out.println("Dit is opdracht 1");
     }
 }
